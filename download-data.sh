70e09ce35af15373331feeee05907272ada9860d
Bing-Chen Tsai <benson1016087@cml18>
2021-06-10 22:07:21 +0800

    Update download-dada.sh for new data

--- download-data.sh
+++ download-data.sh
@@ -1,6 +1,8 @@
 kaggle competitions download -c adl-final-dst-with-chit-chat-seen-domains \
     && unzip -q adl-final-dst-with-chit-chat-seen-domains.zip \
     && rm adl-final-dst-with-chit-chat-seen-domains.zip \
-    && mv data/data/ dataset/ \
+    && mv data/ dataset/ \
+    && mv data-0610/data-0610/ dataset/ \
+    && rm -rf data-0610 \
     && mv seen_empty_submission.csv state_to_csv.py dataset/ \
     && rm -rf data
