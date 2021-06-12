b8e7d45ba840dfce3bf8354887fc00385f4a823f
Wu-Jun Pei <jimpei8989@gmail.com>
2021-06-12 17:04:40 +0800

    Add schema

---
+++ src/datasets/schema.py
@@ -0,0 +1,51 @@
+from dataclasses import dataclass, field
+from typing import List, Dict
+
+from utils.io import json_load
+
+
+@dataclass
+class Slot:
+    name: str
+    description: str
+    is_categorical: bool
+    possible_values: List[str] = field(default_factory=list)
+
+    @property
+    def num_possible_values(self):
+        return len(self.possible_values)
+
+
+@dataclass
+class Service:
+    service_name: str
+    description: str = "No description"
+    slots: List[Slot] = field(default_factory=list)
+    intents: List[Dict] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.slots = [Slot(**s) for s in self.slots]
+
+    @property
+    def name(self):
+        return self.service_name
+
+
+@dataclass
+class Schema:
+    services: List[Service] = field(default_factory=list)
+
+    def __post_init__(self):
+        self.services = [Service(**s) for s in self.services]
+        self.service_by_name = {s.name: s for s in self.services}
+
+    def __iter__(self):
+        for s in self.services:
+            yield(s)
+
+    @classmethod
+    def load_json(cls, json_path):
+        return cls(services=json_load(json_path))
+
+    def get_service(self, name: str) -> Service:
+        return self.service_by_name[name]
--- src/main_dst.py
+++ src/main_dst.py
@@ -1,14 +1,26 @@
 from argparse import ArgumentParser, Namespace
+from pathlib import Path
+
+from datasets.dataset_dst import DSTDatasetForDST
+from datasets.schema import Schema
 from utils.logger import logger
 
 
 def parse_args() -> Namespace:
     parser = ArgumentParser()
+    parser.add_argument("--dataset_dir", type=Path, default="dataset/data/")
     return parser.parse_args()
 
 
 def main(args):
     logger.info(args)
+
+    schema = Schema.load_json(args.dataset_dir / "schema.json")
+    logger.info(
+        "Schema successfully loaded.\n"
+        f"Possible services: {[s.name for s in schema]}\n"
+        f"Sample schema: {schema.services[0]}"
+    )
 
 
 if __name__ == "__main__":
