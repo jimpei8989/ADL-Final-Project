f91285a5e7e0081892388f86db75ca8431c8e496
Bing-Chen Tsai <benson1016087@cml18>
2021-06-12 15:18:14 +0800

    Update prototype of dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -1,12 +1,24 @@
 from torch.utils.data import Dataset
+from pathlib import Path
+import json
+import os
 
 
 class DSTDataset(Dataset):
-    def __init__(self) -> None:
-        pass
+    def __init__(self, json_dir: Path) -> None:
+        self.root = json_dir
+        self.file_names = os.listdir(json_dir)
+        self.data = []
+        for fname in self.file_names:
+            d = json.load(open(self.root / fname, "r"))
+            self.data.append(d)
 
     def __getitem__(self, index: int):
         pass
 
     def __len__(self) -> int:
         pass
+
+
+if __name__ == "__main__":
+    dataset = DSTDataset(Path("dataset/data/train"))
