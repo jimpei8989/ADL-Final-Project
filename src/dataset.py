7286312de0c28e92f027033dfd9df2fc5ae719c7
Bing-Chen Tsai <benson1016087@cml18>
2021-06-12 15:45:37 +0800

    Update prototype of dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -5,13 +5,19 @@ import os
 
 
 class DSTDataset(Dataset):
-    def __init__(self, json_dir: Path) -> None:
+    def __init__(
+        self, json_dir: Path, schema_file: Path, tokenizer=None, max_seq_length=512
+    ) -> None:
         self.root = json_dir
-        self.file_names = os.listdir(json_dir)
+        self.file_names = os.listdir(open(schema_file, "r"))
         self.data = []
+        self.schema_file = json.load(schema_file)
         for fname in self.file_names:
             d = json.load(open(self.root / fname, "r"))
             self.data.append(d)
+
+        self.tokenizer = tokenizer
+        self.max_seq_length = max_seq_length
 
     def __getitem__(self, index: int):
         pass
