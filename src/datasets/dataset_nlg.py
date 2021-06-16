4099b1016c3cc8c5fcb1e0672ced00a02c9d4a68
KeHongYing <b06902074@ntu.edu.tw>
2021-06-16 20:09:29 +0800

    modify nlg_dataset & git ignore

--- src/datasets/dataset_nlg.py
+++ src/datasets/dataset_nlg.py
@@ -1,7 +1,8 @@
 from datasets.dataset import DSTDataset
 from pathlib import Path
 from typing import Iterable, Tuple
-from torch.nn.utils.rnn import pad_sequence
+import random
+
 import torch
 
 
@@ -20,6 +21,8 @@ class DSTDatasetForNLG(DSTDataset):
     ) -> None:
         super().__init__(json_dir, tokenizer, max_seq_length)
         self.nlg_data = []
+        self.history = []
+        self.history_map = {}
 
         for d in self.data:
             for idx, (user, system) in enumerate(pairwise(d["turns"])):
@@ -33,6 +36,8 @@ class DSTDatasetForNLG(DSTDataset):
                     "system_utterance": system["utterance"],
                 }
 
+                self.history.append(tmp)
+
                 if mode == "train":
                     if "beginning" not in system or "end" not in system:
                         continue
@@ -41,6 +46,7 @@ class DSTDatasetForNLG(DSTDataset):
                     tmp["beginning"] = system["beginning"]  # chit-chat begining
                     tmp["end"] = system["end"]  # chit-chat end
 
+                self.history_map[len(self.nlg_data)] = len(self.history)
                 self.nlg_data.append(tmp)
 
     def __getitem__(self, index):
@@ -49,44 +55,66 @@ class DSTDatasetForNLG(DSTDataset):
     def __len__(self):
         return len(self.nlg_data)
 
-    def collate_fn_gen_begin(self, datas):
-        ret = pad_sequence(
-            [
-                torch.LongTensor(self.tokenizer.encode(d["user_utterance"]))
-                for d in datas
-            ],
-            batch_first=True,
-        )
-        return {
-            "dialogue_ids": [f"{d['dialogue_id']}_{d['turns_id'][0]}" for d in datas],
-            "str": [d["user_utterance"] for d in datas],
-            "input_ids": ret,
-        }
+    def classify_collate_fn(self, samples):
+        input_ids = []
+        label = []
+        for sample in samples:
+            idx = self.nlg_data.index(sample)
+            system_utterance = sample["system_utterance"]
+            user_utterance = sample["user_utterance"]
+            chitchat_map = {
+                chat["candidate"]: [chat["label"], 0] for chat in sample["beginning"]
+            }
+            chitchat_map.update(
+                {chat["candidate"]: [chat["label"], 1] for chat in sample["end"]}
+            )
 
-    def collate_fn_gen_end(self, datas):
-        ret = pad_sequence(
-            [
-                torch.LongTensor(
-                    self.tokenizer.encode(d["user_utterance"])
-                    + self.tokenizer.encode(d["system_utterance"])
+            chitchat = list(chitchat_map.keys())[
+                random.randint(0, len(chitchat_map) - 1)
+            ]
+            sentence = (
+                user_utterance
+                + self.tokenizer.sep_token
+                + "".join(
+                    [chitchat, system_utterance][:: (-1) ** chitchat_map[chitchat][1]]
                 )
-                for d in datas
-            ],
-            batch_first=True,
-        )
+                + self.tokenizer.sep_token
+            )
+
+            for dialog in self.history[: self.history_map[idx]]:
+                if len(sentence) > 1024:
+                    break
+                sentence += dialog["system_utterance"]
+                sentence += dialog["user_utterance"]
+
+            input_ids.append(
+                self.tokenizer.encode(
+                    sentence, truncation=True, max_length=512, padding="max_length"
+                )
+            )
+            label.append(0 if chitchat_map[chitchat][0] == "bad" else 1)
+
         return {
-            "dialogue_ids": [f"{d['dialogue_id']}_{d['turns_id'][1]}" for d in datas],
-            "str": [d["system_utterance"] for d in datas],
-            "input_ids": ret,
+            "input_ids": torch.tensor(input_ids, dtype=torch.long),
+            "labels": torch.tensor(label, dtype=torch.float),
         }
 
 
 if __name__ == "__main__":
+    from torch.utils.data import DataLoader
+    from transformers import AutoTokenizer
+
     for paths in ["train", "dev", "test_seen", "test_unseen"]:
         ds = DSTDatasetForNLG(
-            Path("dataset/data") / paths,
-            Path("dataset/data/schema.json"),
-            mode=("test" if "test" in paths else "train"),
+            Path("../dataset/data/data") / paths,
+            mode=("train"),
+            tokenizer=AutoTokenizer.from_pretrained("bert-base-uncased"),
         )
+
+        dataloader = DataLoader(ds, batch_size=1, collate_fn=ds.classify_collate_fn)
+        for d in dataloader:
+            print(d["input_ids"])
+            print(d["labels"])
+            break
 
         print(paths, len(ds))
