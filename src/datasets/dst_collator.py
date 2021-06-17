39e4fb82d94f24318397046d300ab280c7407790
Wu-Jun Pei <jimpei8989@gmail.com>
2021-06-18 01:34:41 +0800

    Add dst collator (needs to be tested)

---
+++ src/datasets/dst_collator.py
@@ -0,0 +1,30 @@
+from typing import Any, Dict, List
+
+from torch.nn.utils.rnn import pad_sequence
+from torch.utils.data._utils.collate import default_collate
+
+
+class DSTCollator:
+    ignored = {"type"}
+
+    def __init__(self, pad_value) -> None:
+        self.pad_value = pad_value
+
+    def __call__(self, batches: List[Dict[str, Any]]) -> Dict[str, Any]:
+        first = batches[0]
+
+        ret = {"type": first["type"]}
+        for key in filter(lambda k: k not in self.ignored, first.keys()):
+            if key == "input_ids":
+                ret.update(
+                    {
+                        key: pad_sequence(
+                            [b[key] for b in batches],
+                            batch_first=True,
+                            padding_value=self.pad_value,
+                        )
+                    }
+                )
+            else:
+                ret.update({key: default_collate([b[key] for b in batches])})
+        return ret
