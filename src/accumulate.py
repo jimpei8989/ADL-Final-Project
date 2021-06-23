b598378ed632fc338f084419abdcdb8713b9a44d
Bing-Chen Tsai <benson1016087@cml18>
2021-06-24 01:40:32 +0800

    Fix bugs

--- src/accumulate.py
+++ src/accumulate.py
@@ -18,20 +18,21 @@ def get_args() -> Namespace:
 def generate_accumulate_jsons(args):
     for data_path in args.input_dir.iterdir():
         ret = []
-        slot_values = defaultdict(dict)
         for dialogue in json.loads(data_path.read_bytes()):
+            slot_values = defaultdict(dict)
             for i, t in enumerate(dialogue["turns"]):
                 if t["speaker"] == "SYSTEM":
                     continue
                 for j, f in enumerate(t["frames"]):
                     # print(f["state"]["slot_values"])
                     slot_values[f["service"]].update(f["state"]["slot_values"])
-                    dialogue["turns"][i]["frames"][j]["state"][
-                        "slot_values"
-                    ] = slot_values[f["service"]]
+                dialogue["turns"][i]["frames"] = [
+                    {"service": k, "state": {"slot_values": dict(v)}}
+                    for k, v in slot_values.items()
+                ]
             ret.append(dialogue)
         args.output_dir.mkdir(parents=True, exist_ok=True)
-        json.dump(ret, open(args.output_dir / data_path.name, "w"))
+        json.dump(ret, (args.output_dir / data_path.name).open("w"), indent=1)
 
 
 if __name__ == "__main__":
