47ab8bb8b4a9ab26e8b2250408f71a4741959e52
Bing-Chen Tsai <benson1016087@cml18>
2021-06-24 00:50:56 +0800

    Update postprocessing

--- src/postprocess.py
+++ src/postprocess.py
@@ -4,11 +4,13 @@ from pathlib import Path
 from nltk import tokenize
 from sentence_transformers import SentenceTransformer
 from tqdm import tqdm
+from collections import defaultdict, Counter
 
 
 def parse_args():
     parser = ArgumentParser()
-    parser.add_argument("--input_file", "-i", type=Path)
+    parser.add_argument("--begin", "-b", type=Path)
+    parser.add_argument("--end", "-e", type=Path)
     parser.add_argument("--output_file", "-o", type=Path)
 
     args = parser.parse_args()
@@ -41,7 +43,9 @@ def sep_sentence(turn):
     return turn
 
 
-def sentence_similarity(model, turn):
+def system_similarity(
+    model, turn
+):  # If chit-chat is too similar as system, then remove
     system_vec = model.encode(turn["system"]).reshape(768, 1)  # (768, )
     for k in ["beginning", "end"]:
         if turn[k] == []:
@@ -55,22 +59,71 @@ def sentence_similarity(model, turn):
     return turn
 
 
-def filtering(res):
+def remove_unrelated(model, turn, threshold=0, versus="system"):
+    target_vec = model.encode(turn[versus]).reshape(768, 1)  # (768, )
+    for k in ["beginning", "end"]:
+        if turn[k] == []:
+            continue
+        chit_vecs = model.encode(turn[k])
+        cos_sim = chit_vecs @ target_vec
+        for s, cos in zip(turn[k], cos_sim):
+            if cos < threshold:
+                # print(versus, turn[versus], "...", s, cos)
+                turn[k].remove(s)
+
+    return turn
+
+
+def filtering(res, threshold=0.1, versus="user"):
     model = SentenceTransformer("LaBSE")
     ret = []
     for r in tqdm(res):
         sep = sep_sentence(r)
-        sentence_similarity(model, sep)
+        sep = system_similarity(model, sep)
+        sep = remove_unrelated(model, sep, threshold, versus)
         ret.append(sep)
 
-    return [
-        {k: " ".join(v) if type(v) is list else v for k, v in r.items()} for r in ret
-    ]
+    return ret
+
+
+def del_duplicate(res, domain="end"):
+    past_rec, cur_dialogue_id = set(), "-1"
+    ret = []
+    for r in res:
+        dialogue_id = "_".join(r["dialogue_ids"].split("_")[:-1])
+        if dialogue_id != cur_dialogue_id:
+            past_rec = set()
+            cur_dialogue_id = dialogue_id
+        for s in r[domain]:
+            if s in past_rec:
+                r[domain].remove(s)
+            past_rec.add(s)
+
+        ret.append(r)
+    return ret
 
 
 if __name__ == "__main__":
     args = parse_args()
-    res = json.load(open(args.input_file, "r"))
+    res_end = filtering(json.load(open(args.end, "r")), threshold=0.05, versus="system")
+    res_end = del_duplicate(res_end, domain="end")
+    res_begin = filtering(
+        json.load(open(args.begin, "r")), threshold=0.1, versus="user"
+    )
 
-    ret = filtering(res)
-    json.dump(ret, open(args.output_file, "w"))
+    ret = defaultdict(dict)
+    for b, e in zip(res_begin, res_end):
+        for key in ["dialogue_ids", "user", "system"]:
+            assert b[key] == e[key]
+        tmp = {
+            "user": b["user"],  # need to be removed
+            "start": " ".join(b["beginning"]),
+            "mod": b["system"],  # need to be empty
+            "end": " ".join(e["end"]),
+        }
+        dialogue_id, idx = (
+            "_".join(b["dialogue_ids"].split("_")[:-1]),
+            int(b["dialogue_ids"].split("_")[-1]) + 1,
+        )
+        ret[dialogue_id][idx] = tmp
+    json.dump(ret, open(args.output_file, "w"), indent=2)
