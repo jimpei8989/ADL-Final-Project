0caacf4c3083610f456da91d478020fb9a9d926a
Wu-Jun Pei <jimpei8989@gmail.com>
2021-06-25 21:55:12 +0800

    Fix dataset_for_prediction

--- src/datasets/dataset_dst_for_prediction.py
+++ src/datasets/dataset_dst_for_prediction.py
@@ -1,8 +1,4 @@
-from bisect import bisect_right
-from collections import defaultdict
-from typing import Tuple, Optional
-
-from transformers import BatchEncoding
+from typing import Any, List
 
 from datasets.dataset_dst import DSTDatasetForDST
 
@@ -11,108 +7,43 @@ class DSTDatasetForDSTForPrediction(DSTDatasetForDST):
     def __init__(
         self,
         *args,
-        test_mode=False,
+        last_user_turn_only: bool = False,
         **kwargs,
     ):
+        self.last_user_turn_only = last_user_turn_only
+
         super().__init__(*args, **kwargs)
 
-        self.test_mode = test_mode
+    def expand(self, dialogue) -> List[Any]:
+        ret = []
 
-        self.dialogue_ids_inv = {did: i for i, did in enumerate(self.dialogue_ids)}
+        turn_indices = (
+            [len(dialogue["turns"]) - 2]
+            if self.last_user_turn_only
+            else range(0, len(dialogue["turns"]), 2)
+        )
 
-        self.another_prefix_sum = [0]
-        self.dialogue_service_slots = defaultdict(list)
-        for i in range(super().__len__()):
-            dialogue, turn_idx = super().__getitem__(i)
-            cnt = 0
+        for turn_idx in turn_indices:
             for service in dialogue["services"]:
                 for slot in self.schema.service_by_name[service].slots:
-                    self.dialogue_service_slots[dialogue["dialogue_id"]].append(
-                        (service, slot.name)
-                    )
-                    cnt += 1
+                    ret.append((turn_idx, service, slot.name))
 
-            self.another_prefix_sum.append(self.another_prefix_sum[-1] + cnt)
+        return ret
 
-    def __len__(self):
-        if self.test_mode:
-            return 10
-        else:
-            return self.another_prefix_sum[-1]
-
-    def __getitem__(self, index: int):
-        # Finds a super_index s.t. APS[super_index] <= index < APS[super_index + 1]
-        super_index = bisect_right(self.another_prefix_sum, index) - 1
-
-        offset = index - self.another_prefix_sum[super_index]
-
-        dialogue, turn_idx = super().__getitem__(super_index)
-        service, slot = self.dialogue_service_slots[dialogue["dialogue_id"]][offset]
-
-        utterance, encoded = self.form_input(
-            turns=dialogue["turns"][: turn_idx + 1],
-            slot_description=self.schema.service_by_name[service].slot_by_name[slot].description,
-            max_length=self.max_seq_length,
-        )
-
-        return {
-            "dialogue_id": dialogue["dialogue_id"],
-            "service": service,
-            "slot": slot,
-            "turn_idx": turn_idx,
-            "utterance": utterance,
-            "encoded": encoded,
-            "input_ids": encoded.input_ids.squeeze(0),
-        }
-
-    def check_item(self, index: int):
+    def check_data(self, dialogue, other):
         return True
 
-    def form_utterance(
-        self,
-        turns,
-        max_length: Optional[int] = None,
-    ):
-        cur_length, turn_idx = 0, len(turns) - 1
-        utterances = []
+    def form_data(self, dialogue, other) -> dict:
+        turn_idx, service_name, slot_name = other
 
-        while turn_idx >= 0:
-            turn = turns[turn_idx]
-            special_token = self.user_token if turn["speaker"] == "USER" else self.system_token
-            utterance = (f"{special_token} " if special_token else "") + turn["utterance"]
+        service = self.schema.service_by_name[service_name]
+        slot = service.slot_by_name[slot_name]
 
-            tokenized = self.tokenizer.tokenize(utterance)
-            if max_length is not None and cur_length + len(tokenized) > max_length:
-                break
-            else:
-                utterances.append(utterance)
-                cur_length += len(tokenized)
-                turn_idx -= 1
-
-        return " ".join(utterances[::-1])
-
-    def form_input(
-        self,
-        turns,
-        slot_description: str,
-        answer: Optional[str] = None,
-        max_length: Optional[int] = None,
-    ) -> Tuple[str, BatchEncoding]:
-        # [CLS] utterance [SEP] slot [SEP] answer [SEP]
-        latter = (
-            slot_description
-            if answer is None
-            else " ".join([slot_description, self.tokenizer.sep_token, answer])
-        )
-        latter_len = len(self.tokenizer.tokenize(latter))
-
-        utterance = self.form_utterance(
-            turns,
-            max_length=(max_length - latter_len if max_length is not None else None),
-        )
-
-        return utterance, self.tokenizer(
-            [utterance], [latter], padding="max_length", return_tensors="pt"
+        return self._form_data(
+            dialogue=dialogue,
+            turns=dialogue["turns"][: turn_idx + 1],
+            latter=f" {self.tokenizer.sep_token} ".join([service.description, slot.description]),
+            max_length=self.max_seq_length,
         )
 
 
@@ -131,6 +62,4 @@ if __name__ == "__main__":
         tokenizer=tokenizer,
     )
     print(len(dataset))
-
-    for i in range(10):
-        print(dataset[i])
+    print(dataset[0])
